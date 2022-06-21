dcfad317878ea2639d9c7b5f1729bfec1c6a506d
Anatoly Obukhov <sapphire.exe@gmail.com>
2022-06-21 23:25:30 +0300

    Fixing JS_2

--- Obukhov_HW_2_JS.js
+++ Obukhov_HW_2_JS.js
@@ -13,7 +13,7 @@
 //  9. Строка не должна быть пустой
 
 //let validstr = prompt('Input from 5 to 64 symbols. Should include at least one uppercase letter, one number and one @'); // for input from prompt window
-let validStr = 'Input from 5 to 64 symbols. Should include at least one uppercase letter, one number and one @'; // for input from code
+let validStr = '  Input from 5 to 64 symbols. Should include   at least one uppercase letter, one number and one @'; // for input from code
 
 console.log(vLength(validStr));
 
