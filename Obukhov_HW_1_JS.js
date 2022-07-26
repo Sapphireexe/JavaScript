a52ccd643be53ee4d3292a25f2d8559fa9c4052b
Anatoly Obukhov <74742029+Sapphireexe@users.noreply.github.com>
2022-07-26 18:16:15 +0300

    Update Obukhov_HW_1_JS.js

--- Obukhov_HW_1_JS.js
+++ Obukhov_HW_1_JS.js
@@ -8,7 +8,7 @@ let item_3; // 7. Создать переменную “item_3”.
 item_3 = item_1 + item_2; // 8. Присвоить переменной item_3 сложение item_1 и item_2.
 console.log(item_3); // 9. Вывести в консоль item_3.
 let item_4; // 10. Создать переменную “item_4”.
-item_4 = 'Yolochka'; // 11. Присвоить переменной item_4 строку “Yolochka”
+item_4 = 'Yolochka'; // 11. Присвоить переменной item_4 строку “Yolochka”.
 console.log(item_4); // 12. Вывести в консоль item_4.
 console.log(item_3 + item_4); // 13. Вывести в консоль сложение item_3 и item_4.
 console.log(item_3 * item_4); // 14. Вывести в консоль умножение item_3 и item_4.
